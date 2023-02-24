665c6832d5349998d4e89dcd0317ad5662fb972c
Tycho Hartingsveld <70889437+99062718@users.noreply.github.com>
2023-02-24 09:37:12 -0800

    added enum packetTypes and packetSizes

---
+++ try1/Core/Networking/Packets/PacketTypes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatChimpClient.Core.Networking.Packets
+{
+    enum packetTypes { 
+        CONNECT = 1,
+        LOGIN = 2,
+        LOGIN_RESULT = 3
+    }
+
+    enum packetSizes {
+        CONNECT = 500,
+        LOGIN = 500,
+        LOGIN_RESULT = 500
+    }
+}
